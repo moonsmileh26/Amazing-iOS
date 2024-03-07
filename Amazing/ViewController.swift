a8017e53b3f7e24515a41ba2a3515586f7b329fa
moonsmileh <moonsmileh@gmail.com>
2024-03-06 23:54:22 -0600

    Initial Commit

---
+++ Amazing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amazing
+//
+//  Created by Humberto Solano on 06/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
